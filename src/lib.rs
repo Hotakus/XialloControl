5a803812625a2e2f6f69c2d847253b4503767149
Hotakus <ttowfive@gmail.com>
2025-07-29 02:31:23 +0800

    feat(lib): create window by rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
+// "windows": [
+//       {
+//         "label": "main",
+//         "title": "XenoControl",
+//         "fullscreen": false,
+//         "decorations": true,
+//         "width": 1080,
+//         "height": 720,
+//         "resizable": false,
+//         "transparent": false,
+//         "maximizable": false
+//       }
+//     ],
+
 use std::env;
-use tauri::Window;
+use tauri::{Manager, Window}; // 👈 引入 Manager 以启用 create_window
+use tauri::{WebviewUrl, WebviewWindowBuilder};
 use tauri_plugin_autostart::{MacosLauncher, ManagerExt};
 
 mod controller;
-mod tray;
 mod setting;
+mod tray;
 mod xeno_utils;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -25,11 +39,16 @@ fn minimize_current_window(window: Window) -> Result<(), String> {
         .map_err(|e| format!("最小化窗口失败: {}", e))
 }
 
-
 #[tauri::command]
 fn open_url(url: &str) -> Result<(), String> {
     tauri_plugin_opener::open_url(url, None::<&str>).map_err(|e| e.to_string())?;
     Ok(())
+}
+
+#[tauri::command]
+fn get_platform() -> String {
+    println!("{:?}", std::env::consts::OS.to_string());
+    std::env::consts::OS.to_string() // "linux" | "windows" | "macos" 等
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -44,17 +63,37 @@ pub fn run() {
             close_current_window,
             minimize_current_window,
             open_url,
-
+            get_platform,
             controller::query_devices,
             controller::use_device,
             controller::disconnect_device,
             controller::set_frequency,
-
             setting::get_current_settings,
             setting::update_settings
         ])
         .setup(|app| {
             let app_handle = app.handle();
+
+            // 判断是否为 Windows 平台
+            #[cfg(target_os = "windows")]
+            let decorations = false;
+            #[cfg(not(target_os = "windows"))]
+            let decorations = true;
+
+            WebviewWindowBuilder::new(
+                &app_handle.clone(),
+                "main",
+                WebviewUrl::App("index.html".into()),
+            )
+            .title("XenoControl")
+            .inner_size(1080.0, 720.0)
+            .resizable(false)
+            .fullscreen(false)
+            .decorations(decorations)
+            .transparent(false)
+            .maximizable(false)
+            .build()?;
+
             let _ = tray::initialize(app_handle.clone());
             let _ = controller::initialize(app_handle.clone());
             let _ = xeno_utils::initialize();
