c04bb4350d448b5edb73a56a2ed1a3677965d12a
Hotakus <ttowfive@gmail.com>
2025-07-31 19:30:24 +0800

    feat: 添加控制器限制结构体及其实现

--- src/controller/datas.rs
+++ src/controller/datas.rs
@@ -13,6 +13,38 @@ pub struct ControllerTrigger {
     pub is_pressed: bool,   // true if pressed, false if released
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
+pub struct ControllerLimits {
+    pub sticks_value_min: f32,
+    pub sticks_value_max: f32,
+    pub triggers_value_min: f32,
+    pub triggers_value_max: f32,
+}
+
+impl ControllerLimits {
+    pub(crate) fn default() -> Self {
+        ControllerLimits {
+            sticks_value_min: -32768.0,
+            sticks_value_max: 32767.0,
+            triggers_value_min: 0.0,
+            triggers_value_max: 255.0,
+        }
+    }
+
+    pub fn set_limits(
+        &mut self,
+        sticks_value_min: f32,
+        sticks_value_max: f32,
+        triggers_value_min: f32,
+        triggers_value_max: f32,
+    ) {
+        self.sticks_value_min = sticks_value_min;
+        self.sticks_value_max = sticks_value_max;
+        self.triggers_value_min = triggers_value_min;
+        self.triggers_value_max = triggers_value_max;
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 pub enum ControllerButtons {
     // Face buttons
@@ -53,6 +85,8 @@ pub struct ControllerDatas {
 
     pub left_stick_center: (f32, f32),
     pub right_stick_center: (f32, f32),
+
+    pub limits: ControllerLimits,
 }
 
 impl ControllerDatas {
@@ -81,6 +115,12 @@ impl ControllerDatas {
             },
             left_stick_center: (0.0, 0.0),
             right_stick_center: (0.0, 0.0),
+            limits: ControllerLimits {
+                sticks_value_min: -1.0,
+                sticks_value_max: 1.0,
+                triggers_value_min: 0.0,
+                triggers_value_max: 255.0,
+            },
         }
     }
 
@@ -108,7 +148,6 @@ impl ControllerDatas {
     pub fn button_is_pressed(&self, button: ControllerButtons) -> bool {
         self.get_button(button)
     }
-
 }
 
 pub fn initialize() {
