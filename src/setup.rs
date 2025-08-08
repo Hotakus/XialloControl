4394e36f9440dff997c5ca5a4bd8baae81216803
Hotakus <ttowfive@gmail.com>
2025-08-08 22:57:07 +0800

    docs: 修改TODO

--- src/setup.rs
+++ src/setup.rs
@@ -5,3 +5,10 @@ use crate::{setting, xeno_utils};
 pub fn setup() {
     xeno_utils::create_config_dir();
 }
+
+
+
+// 1. 软件启动
+// 2. 软件读取设置文件，读取上一次软件使用的预设名
+// 3. 通过上一次预设名，读取预设文件，设置当前预设
+// 4. 前端初始化，调用后端命令，设置当前预设到前端
