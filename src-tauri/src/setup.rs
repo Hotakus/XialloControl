56e1a1056ece91576d63c2e3c8fddb7634b0b2bc
Hotakus <ttowfive@gmail.com>
2025-08-17 16:15:25 +0800

    refactor(setup): 初始化函数添加参数并拆分步骤

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -1,10 +1,15 @@
-use crate::xeno_utils;
+use tauri::AppHandle;
+use crate::{adaptive_sampler, controller, mapping, preset, setting, xeno_utils};
 
-pub fn setup() {
-    xeno_utils::create_config_dir();
+pub fn initialize(app_handle: AppHandle) {
+    xeno_utils::initialize();
+    
+    setting::initialize();
+    
+    preset::initialize();
+    
+    mapping::initialize();
+    controller::initialize(app_handle.clone());
+    
+    adaptive_sampler::initialize();
 }
-
-// 1. 软件启动
-// 2. 软件读取设置文件，读取上一次软件使用的预设名
-// 3. 通过上一次预设名，读取预设文件，设置当前预设
-// 4. 前端初始化，调用后端命令，设置当前预设到前端
